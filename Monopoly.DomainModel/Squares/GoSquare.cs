0544f9489fefccd3427353d75da75fae1a7075f3
cwescott <cwescott@coopervision.com>
2014-07-29 11:06:48 -0400

    Adding missing file.

---
+++ Monopoly.DomainModel/Squares/GoSquare.cs
@@ -0,0 +1,16 @@
+﻿namespace Monopoly.DomainModel.Squares
+{
+    public class GoSquare : Square
+    {
+        public int Salary { get { return 200; } }
+
+        public GoSquare(string name, int index)
+            : base(name, index)
+        { }
+
+        public override void Passed(Player p)
+        {
+            p.AddCash(Salary);
+        }
+    }
+}
